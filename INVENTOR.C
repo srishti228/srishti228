9b0f44683d944603b66387c6e7f133a7c83f1e15
srishti228 <85943698+srishti228@users.noreply.github.com>
2021-07-23 00:23:45 +0530

    Update INVENTOR.C

--- INVENTOR.C
+++ INVENTOR.C
@@ -3,40 +3,35 @@
 
 struct inventory
 {
-    struct inventory *prev;                // Pointer pointing to the previous node
-   // long int item_code;                    // Data as item code
-    char item_name[50];                    // Data as item name
-    int quantity;                          // Quantity of a particular item
-    int price;                           // Price of a particular item
-    struct inventory *next;                // Pointer pointing to the next node
+    struct inventory *prev;                
+    char item_name[50];                  
+    int quantity;                         
+    int price;                          
+    struct inventory *next;                
 };
 typedef struct inventory inv;
-inv *LP=NULL,*temp,*temp1,*temp2,*RP=NULL;   // Globally declaring the pointers to be used in the program
+inv *LP=NULL,*temp,*temp1,*temp2,*RP=NULL;   
 
-void create();          // Creating an empty node
-void insert1();        //  Function to insert item at the beginning
-//void insert2();        // Function to insert item at any position int the list
-//void insert3();       // Function to insert item at the end
-void display();      // Function to display the inventory
-void deletei();     //  Function to remove the item from the inventory
-void savetofile();   // Write items into the file
-void readfromfile(); // Read items from the file
-
+void create();        
+void insert1();      
+void display();     
+void deletei();    
+void savetofile();  
+void readfromfile(); 
 int count = 0;
 
 void main()
 {
     int ch;
 
-   // printf("\n 1 - Create Inventory");
-    printf("\n 2 - Add item to store ");
-  //  printf("\n 3 - Insert item at end");
-//    printf("\n 4 - Insert item at position i");
-    printf("\n 5 - Delete item at i");
-    printf("\n 6 - Display Inventory from beginning");
-    printf("\n 7 - Write items into file");
-    printf("\n 8 - Read items from file");
-    printf("\n 9 - Exit");
+  
+    printf("\n 1 - Add item to store ");
+  
+    printf("\n 2 - Delete item at i");
+    printf("\n 3 - Display Inventory from beginning");
+    printf("\n 4 - Write items into file");
+    printf("\n 5 - Read items from file");
+    printf("\n 6 - Exit");
 
     while (1)
     {
@@ -45,31 +40,23 @@ void main()
 	switch (ch)
 	{
 	case 1:
-	    create();
-	    break;
-	case 2:
 	     insert1();
 	     savetofile();
 	    break;
-//	case 3:
-  //	    insert2();
-//	    break;
- //	case 4:
-//	    insert3();
-  //	    break;
-	case 5:
+
+	case 2:
 	    deletei();
 	    break;
-	case 6:
+	case 3:
 	    display();
 	    break;
-	case 7:
+	case 4:
 	     savetofile();
 	     break;
-	case 8:
+	case 5:
 	    readfromfile();
 	      break;
-	case 9:
+	case 6:
 	    exit(0);
 	     break;
 
@@ -77,10 +64,10 @@ void main()
 	    printf("\n Wrong choice menu");
 	}
     }
-    //return 0;
+   
 }
 
-/* TO create an empty node */
+
 void create()
 {
 
@@ -88,22 +75,18 @@ void create()
     temp =(inv *)malloc(1*sizeof(inv));
     temp->prev = NULL;
     temp->next = NULL;
-    //printf("\n Enter item code : ");
-    // scanf("%ld", &temp->item_code);
+  
     printf("\n Enter name of the item : ");
     scanf("%s",temp->item_name);
     printf("\n Enter the quantity : ");
     scanf("%d",temp->quantity);
     printf("\n Enter the price : ");
     scanf("%d",temp->price);
-   // printf("\n Enter date of manufacture : ");
-   // scanf("%s",temp->dom);
-   // printf("\n Enter expiry date : ");
-   // scanf("%s",temp->ed);
+  
     count++;
 }
 
-/*  TO insert at beginning */
+
 void insert1()
 {
     if (LP == NULL)
@@ -122,68 +105,8 @@ void insert1()
     }
 }
 
-/* To insert at end */
-/*void insert2()
-{
-    if (LP == NULL)
-    {
-	create();
-	LP = temp;
-	temp1 = LP;
-    }
-    else
-    {
-	create();
-	temp1->next = temp;
-	temp->prev = temp1;
-	temp1 = temp;
-	RP=temp;
-	temp->next=NULL;
-    }
-}   */
 
-/* To insert at any position */
-/*void insert3()
-{
-    int pos, i = 2;
 
-    printf("\n Enter position to be inserted : ");
-    scanf("%d", &pos);
-    temp2 = LP;
-
-    if ((pos < 1) || (pos >= count + 1))
-    {
-	printf("\n Position out of range to insert");
-	return;
-    }
-    if ((LP == NULL) && (pos != 1))
-    {
-	printf("\n Empty list cannot insert other than 1st position");
-	return;
-    }
-    if ((LP == NULL) && (pos == 1))
-    {
-	create();
-	LP = temp;
-	temp1 = LP;
-	return;
-    }
-    else
-    {
-	while (i < pos)
-	{
-	    temp2 = temp2->next;
-	    i++;
-	}
-	create();
-	temp->prev = temp2;
-	temp->next = temp2->next;
-	temp2->next->prev = temp;
-	temp2->next = temp;
-    }
-} */
-
-/* To delete an element */
 void deletei()
 {
     int i = 1, pos;
@@ -228,7 +151,7 @@ void deletei()
 	}
 	temp2->next->prev = temp2->prev;
 	if (i != 1)
-	    temp2->prev->next = temp2->next;    /* Might not need this statement if i == 1 check */
+	    temp2->prev->next = temp2->next;   
 	if (i == 1)
 	    LP = temp2->next;
 	printf("\n Node deleted");
@@ -247,13 +170,11 @@ void display()
       }
      while(temp2!=NULL)
      {
-	 // printf("\n%ld",temp2->item_code);
+
 	  printf("\n%s",temp2->item_name);
 	  printf("\n%d",temp2->quantity);
 	  printf("\n%d",temp2->price);
-	 // printf("\n%s",temp2->doa);
-	//  printf("\n%s",temp2->dom);
-	 // printf("\n%s",temp2->ed);
+	
 	  temp2=temp2->next;
 	  printf("\n------------------------------------\n");
      }
@@ -301,13 +222,11 @@ void readfromfile()
     while(fread(&ptr, sizeof(inv), 1, fp) ==1)
     {
 	printf("\nTHE DETAILS WRITTEN INTO THE FILE ARE :\n");
-      //	printf("Item code: %ld\n", ptr->item_code);
+      
 	printf("Item name: %s\n", &ptr->item_name);
 	printf("Quantity: %d\n",&ptr->quantity);
 	printf("Price: %d\n",&ptr->price);
-       // printf("Date of arrival: %s\n",ptr->doa);
-       // printf("Date of manufacture: %s\n",ptr->dom);
-       // printf("Expiry Date: %s\n",ptr->ed);
+     
 	ptr = ptr->next;
     }
 	 fclose(fp);
